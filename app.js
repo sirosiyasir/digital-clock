7516f8e216afd93540662aa111a5ec555b9cc4da
Yasir <112541705+sirosiyasir@users.noreply.github.com>
2022-11-03 19:24:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+/* showTime function'ı oluştur */
+//oluşturduğun function'da new Date()'i çalıştır
+//saati dakikayı ve saniyeyi seç
+//pm ve am'i ayarlar
+//setTimeout oluştur
+// hepsini birleştir ve div'e inner.Text yap
+
+function showTime() {
+  const clock = document.getElementById("MyClockDisplay")
+  let newDate = new Date()
+  let hours = newDate.getHours()
+  let minutes = newDate.getMinutes()
+  let seconds = newDate.getSeconds()
+  let val
+
+  if (hours >= 12) {
+    val = "PM"
+  } else {
+    val = "AM"
+  }
+
+  if (seconds < 9) {
+    seconds = "0" + seconds
+  }
+
+  clock.innerText = hours + " : " + minutes + " : " + seconds + " " + val
+  setTimeout(showTime, 100)
+}
+showTime()
